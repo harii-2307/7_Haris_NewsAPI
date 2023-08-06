b90ab2cc6e76fb58580eab7c4c6b6a6251f01312
harii-2307 <109644553+harii-2307@users.noreply.github.com>
2023-08-06 13:19:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const API_KEY = "2f81b6da98b64eeb9cff1aac55edbf68";
-
-const baseUrl = "https://haris-newsapi.onrender.com/fetchData?q=technology&apiKey=2f81b6da98b64eeb9cff1aac55edbf68";
 const cardsContainer = document.getElementById("cards-container");
 const newsCardTemplate = document.getElementById("template-news-card");
 
@@ -10,9 +7,9 @@ function reload() {
   window.location.reload();
 }
 
-
 async function fetchNews(query) {
-  const url = `${baseUrl}?q=${query}`;
+  const apiKey = process.env.NEWS_API_KEY;
+  const url = `/fetchData?q=${query}&apiKey=${apiKey}`;
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
@@ -20,60 +17,4 @@ async function fetchNews(query) {
 }
 
 
-function bindData(articles) {
-  cardsContainer.innerHTML = "";
-
-  if (Array.isArray(articles) && articles.length > 0) {
-    articles.forEach((article) => {
-      if (!article.urlToImage) return;
-      const cardClone = newsCardTemplate.content.cloneNode(true);
-      fillDataInCard(cardClone, article);
-      cardsContainer.appendChild(cardClone);
-    });
-  } else {
-    const errorMessage = document.createElement("p");
-    errorMessage.textContent = "No news data available.";
-    cardsContainer.appendChild(errorMessage);
-  }
-}
-
-function fillDataInCard(cardClone, article) {
-  const newsImg = cardClone.querySelector("#news-img");
-  const newsTitle = cardClone.querySelector("#news-title");
-  const newsSource = cardClone.querySelector("#news-source");
-  const newsDesc = cardClone.querySelector("#news-desc");
-
-  newsImg.src = article.urlToImage;
-  newsTitle.innerHTML = article.title;
-  newsDesc.innerHTML = article.description;
-
-  const date = new Date(article.publishedAt).toLocaleString("en-US", {
-    timeZone: "Asia/Jakarta",
-  });
-
-  newsSource.innerHTML = `${article.source.name} · ${date}`;
-
-  cardClone.firstElementChild.addEventListener("click", () => {
-    window.open(article.url, "_blank");
-  });
-}
-
-let curSelectedNav = null;
-function onNavItemClick(id) {
-  fetchNews(id);
-  const navItem = document.getElementById(id);
-  curSelectedNav?.classList.remove("active");
-  curSelectedNav = navItem;
-  curSelectedNav.classList.add("active");
-}
-
-const searchButton = document.getElementById("search-button");
-const searchText = document.getElementById("search-text");
-
-searchButton.addEventListener("click", () => {
-  const query = searchText.value;
-  if (!query) return;
-  fetchNews(query);
-  curSelectedNav?.classList.remove("active");
-  curSelectedNav = null;
-});
+// Rest of the code (bindData, fillDataInCard, onNavItemClick, etc.) remains the same.
